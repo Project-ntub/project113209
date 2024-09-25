87efeff9c8c43fff38794326269e712669a52445
Yichieh Chen <11236017@ntub.edu.tw>
2024-09-25 18:40:20 +0800

    settings.py

--- project113209/settings.py
+++ project113209/settings.py
@@ -73,27 +73,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'project113209.wsgi.application'
 
 # Database
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': '113-ntub113209',
-#         'USER': 'ntub113209',
-#         'PASSWORD': 'Sw@23110565',
-#         'HOST': '140.131.114.242',
-#         'PORT': '3306',
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': '113-ntub113209-1',
+        'NAME': '113-ntub113209',
         'USER': 'ntub113209',
         'PASSWORD': 'Sw@23110565',
         'HOST': '140.131.114.242',
         'PORT': '3306',
-        'CONN_MAX_AGE': 300,  # 連接保持5分鐘，設為0則表示每次查詢後關閉連接
     }
 }
+
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
