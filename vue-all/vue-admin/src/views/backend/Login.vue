e129ee9ddeb6917f6d9a827450048fa06714cd65
11236033 <11236033@ntub.edu.tw>
2024-12-13 12:29:22 +0800

    login後台修正

--- vue-all/vue-admin/src/views/backend/Login.vue
+++ vue-all/vue-admin/src/views/backend/Login.vue
@@ -1,4 +1,3 @@
-<!-- backend/login.vue -->
 <template>
   <div class="container">
     <h2>登入</h2>
@@ -27,7 +26,7 @@
     <p>
       忘記密碼? <router-link to="/backend/forgetpassword">重置密碼</router-link>
     </p>
-    <p v-if="error">{{ error }}</p>
+    <p v-if="error" class="error-message">{{ error }}</p>
   </div>
 </template>
 
@@ -53,25 +52,37 @@ export default {
           email: this.email,
           password: this.password,
           remember_me: this.rememberMe,
-          user_type: 'backend'  // 明確指定 user_type 為 backend
+          user_type: 'backend' // 明確指定 user_type 為 backend
         });
-        console.log('登入成功', response);
-        // 儲存 backend_token 而不是 token
-        localStorage.setItem('backend_token', response.data.access);
-        if (this.rememberMe) {
-          localStorage.setItem('refresh_token', response.data.refresh);
-        } else {
-          sessionStorage.setItem('refresh_token', response.data.refresh);
-        }
 
-        // 同時更新 Vuex store
-        this.setBackendToken(response.data.access);
-        await this.fetchPermissions();
-        await this.fetchPreference();
-        this.$router.push('/backend/management');
+        // 確保 response 和 response.data 存在
+        if (response?.data) {
+          console.log('登入成功', response);
+
+          // 儲存 backend_token
+          localStorage.setItem('backend_token', response.data.access);
+          if (this.rememberMe) {
+            localStorage.setItem('refresh_token', response.data.refresh);
+          } else {
+            sessionStorage.setItem('refresh_token', response.data.refresh);
+          }
+
+          // 更新 Vuex store
+          this.setBackendToken(response.data.access);
+          await this.fetchPermissions();
+          await this.fetchPreference();
+          this.$router.push('/backend/management');
+        } else {
+          // 回應結構無效
+          console.error('API 回應格式錯誤', response);
+          this.error = '無法處理後端回應，請稍後再試';
+        }
       } catch (error) {
-        console.error('登入失敗', error.response);
-        this.error = error.response.data.detail || '登入失敗，請檢查您的使用者名稱和密碼';
+        // 處理錯誤情況
+        console.error('登入失敗', error);
+        this.error =
+          error?.response?.data?.detail ||
+          '登入失敗，請檢查您的使用者名稱和密碼';
       }
     }
   }
